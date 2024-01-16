8aef8055a975f25a1283ee22abae5c456b20d3c7
psam1017 <psam1017@naver.com>
2024-01-16 23:56:51 +0900

    jpa auditing 구현, 테스트, 메모 추가

--- src/main/java/portfolio/sunder/global/config/RepositoryConfig.java
+++ src/main/java/portfolio/sunder/global/config/RepositoryConfig.java
@@ -4,9 +4,18 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import portfolio.sunder.global.p6spy.P6SpyEventListener;
 import portfolio.sunder.global.p6spy.P6SpyFormatter;
 
+import java.util.Optional;
+
+@SuppressWarnings("PatternVariableCanBeUsed")
+@EnableJpaAuditing
 @Configuration
 public class RepositoryConfig {
 
@@ -23,5 +32,21 @@ public class RepositoryConfig {
     @Bean
     public P6SpyFormatter p6SpyCustomFormatter() {
         return new P6SpyFormatter();
+    }
+
+    @Bean
+    public AuditorAware<Long> auditorAware() {
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if (authentication != null
+                    && authentication.isAuthenticated()
+                    && authentication.getPrincipal() instanceof UserDetails) {
+
+                UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+                return Optional.of(Long.valueOf(userDetails.getUsername()));
+            }
+            return Optional.empty();
+        };
     }
 }
