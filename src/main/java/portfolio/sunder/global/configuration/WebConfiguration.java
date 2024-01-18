dd2168a8c235bc759ef9a35e7d1475a97b7489a4
psam1017 <psam1017@naver.com>
2024-01-18 23:26:37 +0900

    jwt 에서 subject 추출하는 argument resolver 생성

--- src/main/java/portfolio/sunder/global/configuration/WebConfiguration.java
+++ src/main/java/portfolio/sunder/global/configuration/WebConfiguration.java
@@ -3,12 +3,22 @@ package portfolio.sunder.global.configuration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import portfolio.sunder.global.enumpattern.EnumPatternValidator;
+import portfolio.sunder.global.resolver.argument.UserIdArgumentResolver;
+import portfolio.sunder.infrastructure.jwt.JwtUtils;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Configuration
-public class WebConfiguration {
+public class WebConfiguration implements WebMvcConfigurer {
+
+    private final JwtUtils jwtUtils;
 
     @Bean
     public EnumPatternValidator enumPatternValidator() {
@@ -20,5 +30,10 @@ public class WebConfiguration {
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserIdArgumentResolver(jwtUtils));
     }
 }
