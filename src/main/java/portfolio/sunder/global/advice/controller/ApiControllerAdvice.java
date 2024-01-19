15b34bfa7dca534088f6885ae32f011ef42d12ce
psam1017 <psam1017@naver.com>
2024-01-19 21:21:39 +0900

    global controller advice 생성 중

---
+++ src/main/java/portfolio/sunder/global/advice/controller/ApiControllerAdvice.java
@@ -0,0 +1,9 @@
+package portfolio.sunder.global.advice.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "portfolio.sunder.web")
+public class ApiControllerAdvice {
+
+    // TODO: 2024-01-19 implement and memo
+}
